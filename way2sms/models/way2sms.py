0753bb0ae0ed2445930b7bd03a938eafe19afa63
Annadurai <30859706+annaduraiVignesh@users.noreply.github.com>
2017-09-20 11:39:07 +0530

    removed print statements

--- way2sms/models/way2sms.py
+++ way2sms/models/way2sms.py
@@ -35,18 +35,13 @@ class way2sms(models.Model):
     def sendSms(self):
         url ='http://site24.way2sms.com/Login1.action?'
         data = 'username='+self.user_acc.user_name+'&password='+self.user_acc.password+'&Submit=Sign+in'
- 
         cj= cookielib.CookieJar()
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
- 
         opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120')]
         try:
             usock =opener.open(url, data)
         except IOError:
-            print "error"
             raise Warning(_("Error pls Check account or mobile number"))
-        #return()
- 
         jession_id =str(cj).split('~')[1].split(' ')[0]
         send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
         send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+self.number+'&message='+self.message+'&msgLen=136'
@@ -54,13 +49,9 @@ class way2sms(models.Model):
         try:
             sms_sent_page = opener.open(send_sms_url,send_sms_data)
         except IOError:
-            print "error"
             raise Warning(_("Error pls Check account or mobile number"))
-        #return()
         self.date_time = str(datetime.now())
         raise Warning(_("Message Sent"))
-        print "success"
-    #return ()
 
     @api.one
     @api.constrains('number')
